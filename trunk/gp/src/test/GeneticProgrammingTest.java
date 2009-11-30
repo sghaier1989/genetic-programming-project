1c53a8aabd2aab801698b3ef6243eae1f7f0fa76
v2cib198 <v2cib198@b9f86204-c8e8-11de-b6dc-25c59a28ecba>
2009-11-30 23:46:40 +0000

    changes to single test

--- trunk/gp/src/test/GeneticProgrammingTest.java
+++ trunk/gp/src/test/GeneticProgrammingTest.java
@@ -27,51 +27,12 @@ public class GeneticProgrammingTest extends TestCase {
 		final int hieghtOfTree = 4;
 		final int maxHeight = 5;
 		long startTime = System.currentTimeMillis();
-		long endTime = 0;
 		final TerminalSet terminalSet = TestHelper.getTerminalSet();
 		final FunctionalSet functionalSet = TestHelper.getFunctionalSet();
-		for (int numOfTs = 100; numOfTs <= 2000; numOfTs = numOfTs + 100) {
-			startTime = System.currentTimeMillis();
-			createAndRun(startTime, terminalSet, functionalSet,
-					maxTrainingData, minTrainingData, mutation, crossover,
-					hieghtOfTree, maxHeight, numOfTs);
-			endTime = System.currentTimeMillis();
-			assertTrue("Needs to solve the problem in 15 minutes", endTime
-					- startTime <= 900000);
-		}
-	}
 
-	/**
-	 * Method for running a genetic programming application.
-	 * 
-	 * @param startTime
-	 *            - the start time of the test.
-	 * @param terminalSet
-	 *            - the terminal set.
-	 * @param functionalSet
-	 *            - the functional set.
-	 * @param maxTrainingData
-	 *            - Training data maximum.
-	 * @param minTrainingData
-	 *            - Training data minimum
-	 * @param mutation
-	 *            - Mutation rate of test
-	 * @param crossover
-	 *            - crossover rate of test
-	 * @param hieghtOfTree
-	 *            - starting height of tree
-	 * @param maxHeight
-	 *            - max tree height
-	 * @param numberOfTrees
-	 *            - population/number of trees
-	 */
-	public final synchronized void createAndRun(final long startTime,
-			final TerminalSet terminalSet, final FunctionalSet functionalSet,
-			final int maxTrainingData, final int minTrainingData,
-			final int mutation, final int crossover, final int hieghtOfTree,
-			final int maxHeight, final int numberOfTrees) {
+		startTime = System.currentTimeMillis();
+
 		final FindThread findThread = new FindThread();
-
 		findThread.setGui(false);
 		findThread.setTerminalSet(terminalSet);
 		findThread.setStartTime(startTime);
@@ -82,11 +43,17 @@ public class GeneticProgrammingTest extends TestCase {
 		findThread.setHieghtOfTree(hieghtOfTree);
 		findThread.setMaxRange(maxTrainingData);
 		findThread.setMinRange(minTrainingData);
-		findThread.setNumberOfTrees(numberOfTrees);
+		findThread.setNumberOfTrees(1000);
 		findThread.setMaxHeight(maxHeight);
 		final ThreadGroup threadGroup = new ThreadGroup("FindThread");
 		final Thread thread = new Thread(threadGroup, findThread, "");
 		thread.start();
+
+		while (thread.isAlive()) {
+			// looking
+		}
+		assertTrue("needs to solve in less then 15 minutes", (System
+				.currentTimeMillis() - startTime) < 900000);
 
 	}
 }
